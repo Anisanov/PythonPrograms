0891ba76d73df79768cbecbd30ee14fa8ba5975f
Anisanov <Alanisanov18@gmail.com>
2022-09-27 11:53:24 -0700

    added labels dataframe

--- ConcentrationNormalizer/ConcentrationNormalizer.py
+++ ConcentrationNormalizer/ConcentrationNormalizer.py
@@ -29,16 +29,17 @@ BCA_matrix["Average"] = (BCA_matrix["1"] + BCA_matrix["2"]) / 2
 AverageBlank = (BCA_matrix["11"]["H"] + BCA_matrix["12"]["H"]) / 2
 BCA_matrix["Average - Blank"] = BCA_matrix["Average"] - AverageBlank
 
-# Adding standard concentrations (ug/mL)
-StandardConcentrations = np.array([2000, 1500, 1000, 750, 500, 250, 125, 25])
-
 # Solving linear regression for standard curve
 AvgMinusBlank = np.array(BCA_matrix["Average - Blank"].values).reshape((-1, 1))
-#print(StandardConcentrations)
-#print(AvgMinusBlank)
+StandardConcentrations = np.array([2000, 1500, 1000, 750, 500, 250, 125, 25])
+
+# Plotting Linear fit
 plt.scatter(AvgMinusBlank, StandardConcentrations)
 plt.show()
 model = LinearRegression().fit(AvgMinusBlank, StandardConcentrations)
 
-print(model.coef_)
-print(model.intercept_)
+# Adding Labels to each sample from excel file
+Label_Matrix = df.iloc[[32,33,34,35,36,37,38,39],[14,15,16,17,18,19,20,21,22,23,24,25]]
+Label_Matrix.columns = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+Label_Matrix.index = ["A", "B", "C", "D", "E", "F", "G", "H"]
+print(Label_Matrix)
